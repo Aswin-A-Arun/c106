0f0cd8b795acc5b57951c89a25e911c043f02935
Enygma <57713995+Aswin-A-Arun@users.noreply.github.com>
2020-12-11 18:09:36 +0530

    Add files via upload

---
+++ code106.py
@@ -0,0 +1,25 @@
+import csv 
+import numpy as np 
+
+def getDataSource(data_path):
+        marks_in_percentage=[]
+        days_present=[]
+        with open(data_path) as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                for row in csv_reader:
+                        marks_in_percentage.append(float(row["marks in percentage"]))
+                        days_present.append(float(row["days present"]))
+
+        return {"x":marks_in_percentage, "y":days_present}
+
+def findCorrelation(datasource):
+        correlation = np.corrcoef(datasource["x"],datasource["y"])
+        print("correlation between marks in percentage and days present :-  \n- - - >", correlation [0,1])
+
+def setup():
+        data_path = './data/Students Marks vs Days present.csv'
+
+        datasource = getDataSource(data_path)
+        findCorrelation(datasource)
+
+setup()
